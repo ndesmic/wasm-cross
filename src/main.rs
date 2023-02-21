df599905f253e126d7612f14e5934076708609ee
ndesmic <ndesmic@duck.com>
2023-02-20 19:03:46 -0800

    browser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-pub fn main() {
-    println!("Hello World")
+use std::io;
+use std::env;
+
+pub fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+
+    let foo_var = env::var("FOO").unwrap_or("[foo]".to_string());
+
+    println!("Hello {} {}", buffer, foo_var);
+    Ok(())
 }
